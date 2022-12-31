351e2cb78ee167c40da339fa404ed70af095627a
Richard Pianka <richardpianka@gmail.com>
2022-12-30 22:36:11 -0500

    feat(bus): adding local messages

--- src/MessageBus.ts
+++ src/MessageBus.ts
@@ -3,4 +3,7 @@ export type MessageSubscription = (...args: unknown[]) => void
 export interface MessageBus {
     on(channel: string, callback: MessageSubscription): void
     send(channel: string, ...args: unknown[]): void
+
+    onLocal(channel: string, callback: MessageSubscription): void
+    sendLocal(channel: string, ...args: unknown[]): void
 }
